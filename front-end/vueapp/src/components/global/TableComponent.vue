0b8e549a604547d1b894da3225f0e99f1e79e427
ThiagoFragaK <thiagofraga@proton.me>
2025-07-04 09:45:53 -0300

    Add: global table component

---
+++ front-end/vueapp/src/components/global/TableComponent.vue
@@ -0,0 +1,176 @@
+<template>
+    <div class="table-div">
+        <p class="mx-2 my-2">
+            <small>
+                {{ $t(modalName) }} ({{ totalRows }})
+            </small>
+        </p>
+        <table 
+            class="table table-hover table-light table-sm table-responsive mt-2 mb-2 custom-table"
+        >
+            <thead>
+                <tr>
+                    <th v-if="hasSelection">
+                        <input
+                            type="checkbox"
+                            class="form-check-input"
+                            :checked="allSelected"
+                            @change="selectAllRow"
+                        />
+                    </th>
+                    <th 
+                        v-for="(column, index) in columns" 
+                        :key="index" 
+                    >
+                        {{ $t(column.label) }}
+                    </th>
+                </tr>
+            </thead>
+            <tbody v-if="isLoading">
+                <tr>
+                    <td 
+                        :colspan="columns.length + (hasSelection ? 1 : 0)" 
+                        class="text-center text-primary bg-primary/5 py-4 italic"
+                    >
+                        <div class="d-flex justify-content-center">
+                            <div class="spinner-border" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+            <tbody v-else-if="data.length > 0">
+                <tr
+                    v-for="(row, index) in data" 
+                    :key="index"
+                >
+                    <td v-if="hasSelection">
+                        <input
+                            type="checkbox"
+                            class="form-check-input"
+                            :value="row"
+                            :checked="isSelected(row)"
+                            @change="selectRow(row)"
+                        />
+                    </td>
+                    <td 
+                        v-for="column in columns" 
+                        :key="column.key"
+                    >
+                        <slot 
+                            :name="`cell-${column.key}`" 
+                            :data="{ row, column }" 
+                        >
+                            {{ row[column.key] }}
+                        </slot>
+                    </td>
+                </tr>
+            </tbody>
+            <tbody v-else>
+                <tr>
+                    <td 
+                        :colspan="columns.length + (hasSelection ? 1 : 0)" 
+                        class="text-center text-primary bg-primary/5 py-4 italic"
+                    >
+                        No data available.
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+  
+<script>
+    export default {
+        props: {
+            modalName: {
+                type: String,
+                required: true
+            },
+            totalRows: {
+                type: Number,
+                required: true,
+            },
+            data: {
+                type: Array,
+                required: true,
+            },
+            columns: {
+                type: Array,
+                required: true,
+            },
+            isLoading: {
+                type: Boolean,
+                default: true,
+            },
+            hasSelection: {
+                type: Boolean,
+                default: true,
+            },
+        },
+        data() {
+            return {
+                selectedRows: [],
+            };
+        },
+        methods: {
+            selectAllRow() {
+                if (this.allSelected) {
+                    this.selectedRows = [];
+                } else {
+                    this.selectedRows = [...this.data];
+                }
+                this.$emit("selectedRows", this.selectedRows);
+            },
+            selectRow(row) {
+                const index = this.selectedRows.indexOf(row);
+                if (index === -1) {
+                    this.selectedRows.push(row);
+                } else {
+                    this.selectedRows.splice(index, 1);
+                }
+                this.$emit("selectedRows", this.selectedRows);
+            },
+            isSelected(row) {
+                return this.selectedRows.includes(row);
+            },
+            cleanSelection() {
+                this.selectedRows = [];
+            }
+        },
+        computed: {
+            allSelected() {
+                return this.data.length > 0 && this.selectedRows.length === this.data.length;
+            },
+        },
+        mounted() {
+            this.cleanSelection();
+        },
+    };
+</script>
+
+<style>
+.custom-table {
+  overflow: hidden;
+}
+
+.custom-table thead th {
+  border-bottom: 1px solid #d3d3d3 !important;
+}
+
+.custom-table th,
+.custom-table td {
+  padding: 12px;
+  vertical-align: middle;
+  font-size: 14px;
+  font-weight: 500;
+  color: #343a40;
+}
+
+.table-div {
+    border: 1px solid #d3d3d3;
+    border-radius: 8px;
+    overflow: hidden;
+}
+</style>
