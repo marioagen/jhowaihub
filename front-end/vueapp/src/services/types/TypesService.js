a051fd2c38971cfd3e3610501d2b8f425806f141
ThiagoFragaK <thiagofraga@proton.me>
2025-07-04 14:20:41 -0300

    refactor: divider in typeservice

--- front-end/vueapp/src/services/types/TypesService.js
+++ front-end/vueapp/src/services/types/TypesService.js
@@ -1,19 +1,19 @@
 import api from "@/services/api";
-import paginationDivider from "@/utils/paginationDivider";
+import PaginationDivider from "@/utils/paginationDivider";
+const divider = new PaginationDivider();
 
 export default {
     getTypes(params) {
         return api.get('/TypeDoc/Paged/', { params: params })
             .then(({ data }) => {
-                console.log(data)
                 return {
                     content: data.content,
                     pagination: {
                         currentPage: data.currentPage,
                         pageCount: data.pageCount,
                         rowCount: data.rowCount,
-                        listPage: paginationDivider.calculatePageCount(data.pageCount, data.currentPage)
-                    },   
+                        listPage: divider.calculatePageCount(data.pageCount, data.currentPage),
+                    }
                 }
             }).catch(function (e) {
                 console.log(e);
