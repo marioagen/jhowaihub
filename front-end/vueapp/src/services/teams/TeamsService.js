c54f7f3ae01dc2f9fa56cc403f595fe0ff428f34
ThiagoFragaK <thiagofraga@proton.me>
2025-07-09 13:45:25 -0300

    Add: Team service

---
+++ front-end/vueapp/src/services/teams/TeamsService.js
@@ -0,0 +1,31 @@
+import api from "@/services/api";
+import PaginationDivider from "@/utils/paginationDivider";
+const divider = new PaginationDivider();
+
+export default {
+    getTeams(params) {
+        return api.get('/Team/Paged/', { params: params })
+            .then(({ data }) => {
+                return {
+                    content: data.content,
+                    pagination: {
+                        currentPage: data.currentPage,
+                        totalPages: data.pageCount,
+                        rowCount: data.rowCount,
+                        totalItems: divider.calculatePageCount(data.pageCount, data.currentPage)
+                    }
+                }
+            }).catch(function (e) {
+                console.log(e);
+            });
+    },
+    deleteTeamById(teamId) {
+        return api.delete('/Team/DeleteByIds', { data: [teamId] })
+            .then(() => {
+                return true;
+            }).catch(function (e) { 
+                console.log(e);
+                return false;
+            });
+    },
+}
