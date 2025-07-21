558c7102667182faaa43c4beb39fe01224feb172
ThiagoFragaK <thiagofraga@proton.me>
2025-07-21 14:51:45 -0300

    Refactor: remove unused code in type page

--- front-end/vueapp/src/pages/types.vue
+++ front-end/vueapp/src/pages/types.vue
@@ -30,19 +30,20 @@
                 <TypesTable ref="TypesTable" @toast="handleToast" />
             </div>
 
-            <modal-form v-if="modalAlertShow" :dataEditing="dataModal" @openAdd="addType" @close="closeModal" />
-            <toast-alert :showToast="toastShow" :colorToast="toastColor" :messageToast="toastMessage" @close="closeToast" />
+            <TypesModal
+                :isEdit="false"
+                :type="modalType"
+                @reload="$refs.TypesTable.getTypes()"
+                ref="TypesModal"
+            />
         </div>
     </main>    
 </template>
 
 <script>
-    import ModalForm from "@/components/pages/type/modal-form";
-    import ToastAlert from "@/components/common/toast-alert";
-    import paginationDivider from "@/utils/paginationDivider";
     import TypesTable from "@/components/types/TypesTable.vue";
-    import TypesService from "@/services/types/TypesService";
     import SearchComponent from "@/components/global/SearchComponent.vue";
+    import TypesModal from "@/components/types/TypesModal.vue";
 
     export default {
         name: "TypeManager",
@@ -52,36 +53,17 @@
                 crumbsData: [],
                 entitySearch: {},
                 resetInputSearch: false,
-                sidebarData: "Type",
-                queryPage: this.$route.query.page ? this.$route.query.page : 1,
-                searchInput: "",
-                searching: false,
-                dataType: [],
-                loading: false,
-                pagination: { currentPage: 0, pageCount: 0, rowCount: 0, listPage: 0 },
-                modalAlertShow: false,
-                modalEntity: {},
-                isAscending: false,
-                dataModal: {},
-                colType: 2,
-                selectedOption: 10,
-                toastShow: false,
-                toastColor: "",
-                toastMessage: "",
-                divider: new paginationDivider(),
-                listIds: [],
+                modalType: {
+                    name: "",
+                }
             };
         },
         components: {
-            ModalForm,
-            ToastAlert,
-            TypesTable,
             SearchComponent,
+            TypesTable,
+            TypesModal,
         },
         watch: {
-            searchInput: function (val) {
-                this.searching = false;
-            },
             "$store.state.userProfile.language": function () {
                 this.setEntitySearch();
             },
@@ -99,70 +81,7 @@
                 this.$refs.TypesTable.filterList(obj.search);
             },
             openModalType() {
-
-            },
-            addType: function (name) {
-                const self = this;
-                TypesService.addType(name)
-                    .then((result) => {
-                        if (!result.success) {
-                            const messageKey =
-                                result.status === 409 ? "labelDocumentTypeAlreadyExists" : "labelDocumentTypeError";
-
-                            this.alertToast(this.$t(messageKey), "toast-warning");
-                        } else {
-                            self.$refs.TypesTable.getTypes({ search: "", page: self.queryPage, type: null });
-                            self.resetInputSearch = !self.resetInputSearch;
-                            self.alertToast(self.$t("labelDocumentTypeSuccess"), "toast-success");
-                        }
-                    })
-                    .finally(() => {
-                        console.log("Finished request.");
-                    });
-            },
-            confirmationDialog: function (item) {
-                this.modalEntity = item;
-                this.modalAlertShow = true;
-                document.getElementsByTagName("BODY")[0].children[1].className += " active";
-            },
-            openModal: function (data = null) {
-                this.modalAlertShow = true;
-                if (data) this.dataModal = data;
-                document.getElementsByTagName("BODY")[0].children[1].className += " active";
-            },
-            closeModal: function () {
-                this.modalAlertShow = false;
-                this.modalAlertShow = false;
-                document.getElementsByTagName("BODY")[0].children[1].className = "overlay";
-            },
-            upperFormat: function (str) {
-                return str.toUpperCase();
-            },
-            alertToast(msg, color) {
-                this.clearMyInterval();
-                this.toastMessage = msg;
-                this.toastColor = color;
-                this.toastShow = true;
-
-                this.myInterval = setTimeout(() => {
-                    this.toastMessage = "";
-                    this.toastColor = "";
-                    this.toastShow = false;
-                    this.myInterval = null;
-                }, 4000);
-            },
-            closeToast: function () {
-                this.toastShow = false;
-                this.clearMyInterval();
-            },
-            clearMyInterval() {
-                if (this.myInterval) {
-                    clearTimeout(this.myInterval);
-                    this.myInterval = null;
-                }
-            },
-            handleToast({ message, color }) {
-                this.alertToast(message, color);
+                this.$refs.TypesModal.open();
             },
         },
         created() {
