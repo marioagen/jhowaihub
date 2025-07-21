001d63cd6a1d36fe0b40f52876d2e511a9ec1cee
Gabriel <gtoliveira4@latam.stefanini.com>
2025-07-21 11:29:47 -0300

    Modified   back-end/WoopiAiHub.Domain/Models/Document.cs Modified   back-end/WoopiAiHub.Domain/Models/Permission.cs Modified   back-end/WoopiAiHub.Domain/Models/Profile.cs Modified   back-end/WoopiAiHub.Domain/Models/Team.cs Modified   back-end/WoopiAiHub.Domain/Models/User.cs Modified   back-end/WoopiAiHub.Repository/Mappings/DocumentMap.cs Modified   back-end/WoopiAiHub.Repository/Migrations/ApplicationDbContextModelSnapshot.cs Added      back-end/WoopiAiHub.Repository/Migrations/20250721142802_AddDocumentTeams.Designer.cs Added      back-end/WoopiAiHub.Repository/Migrations/20250721142802_AddDocumentTeams.cs

---
+++ back-end/WoopiAiHub.Repository/Migrations/20250721142802_AddDocumentTeams.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WoopiAiHub.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDocumentTeams : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DocumentTeams",
+                columns: table => new
+                {
+                    TeamId = table.Column<int>(type: "int", nullable: false),
+                    DocumentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocumentTeams", x => new { x.TeamId, x.DocumentId });
+                    table.ForeignKey(
+                        name: "FK_DocumentTeams_Documents_DocumentId",
+                        column: x => x.DocumentId,
+                        principalTable: "Documents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DocumentTeams_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocumentTeams_DocumentId",
+                table: "DocumentTeams",
+                column: "DocumentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DocumentTeams");
+        }
+    }
+}
