6d20e3f97795a4902feca989602f9722bdbab4e0
ThiagoFragaK <thiagofraga@proton.me>
2025-07-22 11:50:05 -0300

    Add: notification util

--- front-end/vueapp/src/utils/notification.js
+++ front-end/vueapp/src/utils/notification.js
@@ -1,12 +1,6 @@
-export function notify({ 
-    title, 
-    message, 
-    variant = 'primary', 
-    icon = null, 
-    duration = 3000 
-}) {
-    const event = new CustomEvent('notification:show', {
-        detail: { title, message, variant, icon, duration }
-    })
-    window.dispatchEvent(event)
+export function notify({ title, message, variant = "primary", icon = null, duration = 3000 }) {
+    const event = new CustomEvent("notification:show", {
+        detail: { title, message, variant, icon, duration },
+    });
+    window.dispatchEvent(event);
 }
