7cc234882e90e73f1f487dfb50c6cdf03c7f3afe
ThiagoFragaK <thiagofraga@proton.me>
2025-07-22 18:06:10 -0300

    Refactor: Change routes to use new login page

--- front-end/vueapp/src/router/index.js
+++ front-end/vueapp/src/router/index.js
@@ -1,19 +1,18 @@
 ﻿import { createRouter, createWebHashHistory } from "vue-router";
-import LoginIndex from "@/components/pages/login";
+// import LoginIndex from "@/components/pages/login";
 import LogoutIndex from "@/components/pages/logout";
 import DocumentUpload from "@/components/pages/document/upload";
 import DocumentList from "@/components/pages/document/list";
-import TypeManager from "@/components/pages/manager/type";
 import QuestionManager from "@/components/pages/manager/question";
 import QuizFormNew from "@/components/pages/quiz/form-new";
 import QuizFormEdit from "@/components/pages/quiz/form-edit";
 import QuizManager from "@/components/pages/manager/quiz";
 import NormalizeIndex from "@/components/pages/normalize/loading";
 import AnalyzerIndex from "@/components/pages/analyzer";
-import UserIndex from "@/components/pages/user/index";
 
 import TypesPage from "@/pages/types.vue";
 import UserManagePage from "@/pages/user-manager.vue";
+import LoginIndex from "@/pages/login.vue";
 
 function authenticate(to, from, next) {
     var usuario = JSON.parse(window.localStorage.getItem("project"));
