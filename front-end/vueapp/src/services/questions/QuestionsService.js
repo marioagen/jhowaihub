e7989e0a920a77656cccb6ef27d7dfc309731b3f
ThiagoFragaK <thiagofraga@proton.me>
2025-07-25 15:08:11 -0300

    Add: questions service

---
+++ front-end/vueapp/src/services/questions/QuestionsService.js
@@ -0,0 +1,58 @@
+import api from "@/services/api";
+
+export default {
+    getQuestions(params) {
+        return api.get("/Question/Paged", { params: params })
+            .then(({ data }) => {
+                return {
+                    content: data.content,
+                    pagination: {
+                        currentPage: data.currentPage,
+                        totalPages: data.pageCount,
+                        rowCount: data.rowCount,
+                        totalItems: data.rowCount,
+                    }
+                }
+            })
+            .catch((e) => {
+                return {
+                    error: e,
+                }
+            });
+    },
+    createQuestion(description) {
+        return api.post(`/Question?description=${description}`)
+            .then(() => {
+                return true;
+            })
+            .catch(function (e) {
+                let errorMessage = "";
+                if (e.response.status == 409) {
+                    errorMessage = "labelQuestionAlreadyExists";
+                } else {
+                    errorMessage = "labelQuestionError";
+                }
+
+                return {
+                    error: errorMessage,
+                }
+            });
+    },
+    editQuestion(params) {
+        return api.put("/Question", params)
+            .then(() => {
+                return true;
+            })
+            .catch((e) => {
+                let errorMessage = "";
+                if (e.response.status == 409) {
+                    errorMessage = "labelQuestionAlreadyExists";
+                } else {
+                    errorMessage = e;
+                }
+                return {
+                    error: errorMessage,
+                }
+            });
+    }
+}
