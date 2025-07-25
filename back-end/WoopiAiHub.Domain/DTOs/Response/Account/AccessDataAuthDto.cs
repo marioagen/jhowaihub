955809d6d1ffabd3df68b297993a430731ab7095
ThiagoFragaK <thiagofraga@proton.me>
2025-07-25 17:05:05 -0300

    Refactor: replace and rework login dtos

---
+++ back-end/WoopiAiHub.Domain/DTOs/Response/Account/AccessDataAuthDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WoopiAiHub.Domain.DTOs.Response.Account
+{
+    public class AccessDataAuthDto
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public LoginDataDto? Data { get; set; }
+    }
+}
