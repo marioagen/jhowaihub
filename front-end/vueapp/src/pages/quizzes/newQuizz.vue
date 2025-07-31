e4fd123ce993ef53706937e1c83885682c242c8f
ThiagoFragaK <thiagofraga@proton.me>
2025-07-31 20:35:48 -0300

    Add: new quizz page and create edit pages

---
+++ front-end/vueapp/src/pages/quizzes/newQuizz.vue
@@ -0,0 +1,15 @@
+<template>
+    <QuizzesForm 
+        :edit="false"
+    />
+</template>
+
+<script>
+    import QuizzesForm from '@/components/quizzes/QuizzesForm.vue';
+    export default {
+        name: "EditQuizz",
+        components: {
+            QuizzesForm
+        },
+    }
+</script>
