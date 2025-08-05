29b444df9e25598280dd49023915b2e47436970a
ThiagoFragaK <thiagofraga@proton.me>
2025-08-05 17:12:11 -0300

    Refactor: Only clean state if logout is successful

--- front-end/vueapp/src/components/pages/logout/index.vue
+++ front-end/vueapp/src/components/pages/logout/index.vue
@@ -6,30 +6,24 @@ export default {
     name: "Logout",
     beforeRouteEnter: function (to, from, next) {
         AuthService.Logout()
-            .catch(() => {
-                this.$notify({
-                    title: 'Error',
-                    message: this.$t('unexpectedError'),
-                    variant: 'danger',
-                    icon: 'CircleX',
-                });
-            })
-            .finally(() => {
-                document.documentElement.className = to.query.darkMode === "true" ? "css-theme-dark" : "css-theme-light";
-                window.localStorage.removeItem("project");
-                var dataUser = {
-                    language: store.state.userProfile.language,
-                    image: "",
-                    name: "",
-                    login: "",
-                    tokenAzure: "",
-                    tokenApi: "",
-                    tenant: "",
-                    keyMongoAccess: "",
-                };
-                store.commit("updateUserProfile", { amount: dataUser });
-                store.commit("setTenantInitialized", false);
-                next({ path: "/" });
+            .then((status) => {
+                if(status) {
+                    document.documentElement.className = to.query.darkMode === "true" ? "css-theme-dark" : "css-theme-light";
+                    window.localStorage.removeItem("project");
+                    var dataUser = {
+                        language: store.state.userProfile.language,
+                        image: "",
+                        name: "",
+                        login: "",
+                        tokenAzure: "",
+                        tokenApi: "",
+                        tenant: "",
+                        keyMongoAccess: "",
+                    };
+                    store.commit("updateUserProfile", { amount: dataUser });
+                    store.commit("setTenantInitialized", false);
+                    next({ path: "/" });
+                }
             });
     },
 };
