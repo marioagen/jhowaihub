256ecaba1a8167415a2e4de4f073d53be4e07478
ThiagoFragaK <thiagofraga@proton.me>
2025-08-12 10:56:15 -0300

    Add: get profiles endpoint in service

--- front-end/vueapp/src/services/profiles/ProfilesService.js
+++ front-end/vueapp/src/services/profiles/ProfilesService.js
@@ -19,6 +19,17 @@ export default {
                 console.log(e);
             });
     },
+    getProfilesList() {
+        return api.get("/Profile/FindAll")
+            .then(({ data }) => {
+                return data;
+            })
+            .catch((error) => {
+                return {
+                    error: error,
+                }
+            });
+    },
     deleteProfileById(ids) {
         return api
             .delete("/Profile", { data: ids })
