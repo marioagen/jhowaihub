8f925e968e075e8630ae8438bb6158f10e7f2bee
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2025-08-13 08:10:43 -0300

    Update AccountServices.cs

--- back-end/WoopiAiHub.Application/Services/AccountServices.cs
+++ back-end/WoopiAiHub.Application/Services/AccountServices.cs
@@ -294,7 +294,7 @@ namespace WoopiAiHub.Application.Services
             var token = new JwtSecurityToken(_config["Jwt:Issuer"],
                 _config["Jwt:Audience"],
                 claims,
-                expires: DateTime.Now.AddMinutes(55),
+                expires: DateTime.Now.AddMinutes(5),
                 signingCredentials: credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
