27ff870e6e429d889228f52aae736588a3e04a63
ThiagoFragaK <thiagofraga@proton.me>
2025-08-19 16:33:31 -0300

    refactror: remove details

--- front-end/vueapp/src/services/workflow/WorkflowService.js
+++ front-end/vueapp/src/services/workflow/WorkflowService.js
@@ -8,7 +8,7 @@ export default {
             })
             .catch((error) => {
                 return {
-                    error: error.response.data.detail,
+                    error: error?.response?.data?.detail,
                 }
             });
     },
@@ -30,7 +30,7 @@ export default {
             })
             .catch((error) => {
                 return {
-                    error: error.response.data.detail,
+                    error: error?.response?.data?.detail,
                 }
             });
     },
