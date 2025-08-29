5fbeeaacfd65382419350cedd813037c612ec99a
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2025-08-29 16:04:43 -0300

    Update Card.cs

--- back-end/WoopiAiHub.Domain/Models/Card.cs
+++ back-end/WoopiAiHub.Domain/Models/Card.cs
@@ -15,8 +15,6 @@ namespace WoopiAiHub.Domain.Models
 
         [Column("StatusId", TypeName = "int")]
         public int StatusId { get; private set; }
-        [Column("Enable", TypeName = "bit")]
-        public bool Enable { get; set; }
 
         [Column("Enable", TypeName = "bit")]
         public bool Enable { get; private set; }
