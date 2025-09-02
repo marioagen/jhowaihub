46cfb8788d3e2135dcc42e3afd6b9cf54a553a43
ThiagoFragaK <thiagofraga@proton.me>
2025-09-02 15:36:47 -0300

    refactor: remove spaces

--- front-end/vueapp/src/pages/documents/index.vue
+++ front-end/vueapp/src/pages/documents/index.vue
@@ -14,7 +14,6 @@
                         {{ $t("documents.createBtn") }}
                     </button>
                 </div>
-
                 <div class="card mb-3">
                     <div class="card-body">
                         <div class="d-flex align-items-center gap-2 flex-wrap">
@@ -44,7 +43,6 @@
                     </div>
                 </div>
             </div>
-
             <DocumentsTable 
                 ref="DocumentsTable"
             />
