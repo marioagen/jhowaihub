02be856d58389062d615a31244ec50c0798bed57
ThiagoFragaK <thiagofraga@proton.me>
2025-09-03 14:41:23 -0300

    add: workflow post dto

---
+++ back-end/WoopiAiHub.Domain/DTOs/Request/WorkflowFilterDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoopiAiHub.Domain.DTOs.Request
+{
+    public record class WorkflowFilterDto
+    {
+        public string Input { get; set; } = string.Empty;
+        public Boolean IsAllUsers { get; set; } = false;
+    }
+}
