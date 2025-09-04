368c47ddc668eaf303270d835f663fb47fcb4d79
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2025-09-04 09:55:24 -0300

    Times com workflow

--- back-end/WoopiAiHub.Domain/DTOs/Response/TeamDto.cs
+++ back-end/WoopiAiHub.Domain/DTOs/Response/TeamDto.cs
@@ -6,5 +6,6 @@
         public string Name { get; set; } = string.Empty;
         public DateTime Created { get; set; }
         public IEnumerable<UserDto> Users { get; set; } = Enumerable.Empty<UserDto>();
+        public WorkflowDto? Workflow { get; set; }
     }
 }
