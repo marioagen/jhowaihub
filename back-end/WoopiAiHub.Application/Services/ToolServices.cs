2b9af3470c9f69b2b2842b625583b23464534290
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2025-09-12 09:20:58 -0300

    Testes

--- back-end/WoopiAiHub.Application/Services/ToolServices.cs
+++ back-end/WoopiAiHub.Application/Services/ToolServices.cs
@@ -146,8 +146,8 @@ namespace WoopiAiHub.Application.Services
 
             if (!string.IsNullOrEmpty(pagedDataDto.Search))
             {
-                totalList = totalList.Where(i => EF.Functions.Like(i.Name, $"%{pagedDataDto.Search}%") ||
-                                                 EF.Functions.Like(i.Id.ToString(), $"%{pagedDataDto.Search}%"));
+                totalList = totalList.Where(i => i.Name.ToLower().Contains(pagedDataDto.Search.ToLower()) ||
+                                                 i.Id.ToString().Contains(pagedDataDto.Search));
             }
 
             var totalListCount = totalList.Count();
---
+++ tests/WoopiAiHub.UnitTests/Services/ToolTypeServicesTests.cs
@@ -0,0 +1,43 @@
+﻿using Google.Api;
+using Moq;
+using Moq.AutoMock;
+using WoopiAiHub.Application.Services;
+using WoopiAiHub.Domain.Interfaces.Repository;
+using WoopiAiHub.Domain.Interfaces.Services;
+using WoopiAiHub.UnitTests.Fixture;
+using Xunit;
+
+namespace WoopiAiHub.UnitTests.Services
+{
+    [Collection(nameof(ToolTypeCollection))]
+    public class ToolTypeServicesTests
+    {
+        private readonly AutoMocker _mocker;
+        private readonly IToolTypeServices _toolTypeServices;
+        private readonly Mock<IToolTypeRepository> _toolTypeRepositoryMock;
+
+        public ToolTypeServicesTests()
+        {
+            _mocker = new AutoMocker();
+            _toolTypeServices = _mocker.CreateInstance<ToolTypeServices>();
+            _toolTypeRepositoryMock = _mocker.GetMock<IToolTypeRepository>();   
+        }
+
+        [Fact(DisplayName = "FindAllAsync should return all tool types")]
+        [Trait("FindAllAsync", "Success")]
+        public async Task FindAllAsync_ShouldReturnAllToolTypes()
+        {
+            // Arrange
+            var toolTypes = ToolTypeFixture.FindValidToolTypes();
+            _toolTypeRepositoryMock.Setup(x => x.FindAllAsync()).ReturnsAsync(toolTypes);
+
+            // Act
+            var result = await _toolTypeServices.FindAllAsync();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(toolTypes, result);
+            _toolTypeRepositoryMock.Verify(x => x.FindAllAsync(), Times.Once);
+        }
+    }
+}
