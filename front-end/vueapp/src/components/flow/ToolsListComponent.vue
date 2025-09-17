8626218fe0387405a3e897b49d5c4f820b41459f
ThiagoFragaK <thiagofraga@proton.me>
2025-09-17 15:45:28 -0300

    add: toollist component

---
+++ front-end/vueapp/src/components/flow/ToolsListComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button
+            v-for="tool in toolsList"
+            :key="tool.id"
+            class="btn btn-outline-primary btn-sm me-2"
+        >
+            {{ tool.name }}
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ToolsList",
+        data: () => ({
+            toolsList: [
+                { id: 0, name: "OCR" },
+                { id: 1, name: "OCR II" },
+                { id: 2, name: "OCR III" },
+                { id: 3, name: "OCR IV" },
+                { id: 4, name: "OCR V" },
+            ],
+        }),
+    }
+</script>
