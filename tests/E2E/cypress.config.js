8baa06726cf46e46e191f0b12dffc820ed368598
Luis <lfmarques@stefanini.com>
2025-09-18 15:26:01 -0300

    Testes automatizados - Cypress

---
+++ tests/E2E/cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+const env = require("./cypress.env.json");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: env.baseUrl
+  },
+      defaultCommandTimeout: 20000,
+      requestTimeout: 20000,
+      responseTimeout: 20000,
+});
