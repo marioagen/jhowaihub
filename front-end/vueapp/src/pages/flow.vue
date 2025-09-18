d1c0a1f657f78c430bfa0b70e45140a8b76e4879
brendaLessa <blalmeida@latam.stefanini.com>
2025-09-18 10:10:31 -0300

    first-commit

--- front-end/vueapp/src/pages/flow.vue
+++ front-end/vueapp/src/pages/flow.vue
@@ -10,16 +10,48 @@
                 <div class="card mb-3">
                     <div class="card-body vue-flow-container p-0">
                         <VueFlow :nodes="nodes" :edges="edges" :style="{ width: '100%', height: '100%' }" @connect="onConnect">
-                            <Background  patternColor="#BCD5F2" gap="10" variant="dots" size="1"/>
+                            <Background patternColor="#BCD5F2" gap="10" variant="dots" size="1" />
                             <template #node-hub="props">
-                                <HubNode :node="props" @deleteNode="deleteNode" @openNodeConfig="openNodeConfig"/>
+                                <HubNode :node="props" @deleteNode="deleteNode" @openNodeConfig="openNodeConfig" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"/>
                             </template>
                             <template #edge-special="props">
-                                <SpecialEdge v-bind="props" @deleteEdge="deleteEdge" :data="props"/>
+                                <SpecialEdge v-bind="props" @deleteEdge="deleteEdge" :data="props" />
                             </template>
                         </VueFlow>
                     </div>
-                </div>               
+                </div>
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                    <div class="offcanvas-header">
+                        <h5 id="offcanvasRightLabel">Configurar I/O: nomeFerramenta</h5>
+                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <div class="mb-3">
+                            <h6>Inputs</h6>
+                            <hr>
+                            <div class="background-div">
+                                <p class="mb-2">Receber de: nomedoNodoAnterior</p>
+                                <span class="text-sm">Selecione o output de origem:</span>
+                                <select class="form-select form-select-sm mt-2" aria-label="Default select example">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <h6>Outputs</h6>
+                            <hr>
+                            <div class="background-div">
+                                <span>
+                                    nomeOutput
+                                    <LucideIcon :icon="'Copy'" class="copy" :size="16"/>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
@@ -73,9 +105,6 @@ const { addEdges } = useVueFlow()
             },
             openNodeConfig(nodeId) {
                 const node = this.nodes.find(n => n.id === nodeId);
-                if (node) {
-                    alert(`Open config for node: ${node.label}`);
-                }
             },
             onConnect(params) {
                 this.edges.value = addEdges({ ...params, type: 'step' }, this.edges.value)
@@ -97,4 +126,10 @@ const { addEdges } = useVueFlow()
 .vue-flow-container{
     height: calc(100vh - 200px);
 }
+    .background-div {
+        background-color: rgb(249 250 251);
+        border-width: 1px;
+        border-radius: 0.375rem;
+        padding: 15px;
+    }
 </style>
