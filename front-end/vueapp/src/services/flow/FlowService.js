6bb1819433af18d0c03c5bc0a5fc19604b501b61
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2025-09-18 14:50:01 -0300

    Update FlowService.js

--- front-end/vueapp/src/services/flow/FlowService.js
+++ front-end/vueapp/src/services/flow/FlowService.js
@@ -2,7 +2,7 @@ import api from "@/services/api";
 
 export default {
     getStepToolsByStepId(stepId) {
-        // TODO: Replace with real API call
+        // Replace with real API call
         const stepTools = [
             { Id: 1, ToolId: 201, Label: "OCR", PositionX: 100, PositionY: 100 },
             { Id: 2, ToolId: 202, Label: "Validação", PositionX: 200, PositionY: 100 },
@@ -13,7 +13,7 @@ export default {
     },
 
     getStepToolDependenciesByStepId(stepId) {
-        // TODO: Replace with real API call
+        // Replace with real API call
         const stepToolDependencies = [
             { StepToolIdFrom: 1, StepToolIdTo: 2 },
             { StepToolIdFrom: 2, StepToolIdTo: 3 },
