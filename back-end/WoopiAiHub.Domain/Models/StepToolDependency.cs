814f2a6b54e626e2d43e58b5468329cfe44ca51d
Gabriel <gtoliveira4@latam.stefanini.com>
2025-09-19 14:20:40 -0300

    Added      back-end/WoopiAiHub.Domain/Models/StepTool.cs Added      back-end/WoopiAiHub.Domain/Models/StepToolDependency.cs Added      back-end/WoopiAiHub.Domain/Models/StepToolParameter.cs

---
+++ back-end/WoopiAiHub.Domain/Models/StepToolDependency.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WoopiAiHub.Domain.Models
+{
+    public class StepToolDependency
+    {
+        [Column("DependencyStepToolId", TypeName = "int")]
+        public int DependencyStepToolId { get; set; }
+
+        [Column("DependentStepToolId", TypeName = "int")]
+        public int DependentStepToolId { get; set; }
+
+        public virtual StepTool DependencyStepTool { get; set; } = null!;
+        public virtual StepTool DependentStepTool { get; set; } = null!;
+
+        /// <summary>
+        /// Use to EF context
+        /// </summary>
+        private StepToolDependency() { }
+    }
+}
