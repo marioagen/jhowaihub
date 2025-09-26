8fbdc3ffe8b3bfacb122f445ec298094489a45e8
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2025-09-26 09:20:24 -0300

    automation

--- back-end/WoopiAiHub.Domain/Interfaces/Services/Automation/IAutomationServices.cs
+++ back-end/WoopiAiHub.Domain/Interfaces/Services/Automation/IAutomationServices.cs
@@ -1,23 +1,13 @@
-﻿using WoopiAiHub.Domain.DTOs.Request;
-using WoopiAiHub.Domain.DTOs;
-using WoopiAiHub.Domain.Interfaces.Repository;
-using WoopiAiHub.Domain.Models;
+﻿using WoopiAiHub.Domain.Models;
 
 namespace WoopiAiHub.Domain.Interfaces.Services.Automation
 {
     public interface IAutomationServices
     {
-        void PrepareExecutionAsync(ICollection<Workflow> workflows);
+        bool PrepareExecutionAsync(ICollection<Workflow> workflows);
         Task StartExecutionByWorkflowsAsync(ICollection<Workflow> workflows);
         Task StartExecutionByStepAsync(Step step);
         Task StartExecutionByCardAsync(int stepId, int cardId);
         Task ContinueExecution(int stepToolId, int cardId);
-        ICollection<StepTool> FindStepToolsByStepId(int stepId);
-        ICollection<StepToolDto> FindAll();
-        Task<StepToolDto> FindById(int id);
-        bool DeleteByIds(List<int> ids);
-        Task<bool> Update(int id,
-                          string input);
-        Task<bool> CreateAsync(StepToolCreateDto stepToolCreateDto);
     }
 }
