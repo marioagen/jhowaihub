f8efbf1f36440e70d2ec347c7605a14cd533f60d
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2025-10-16 07:45:52 -0300

    Settings

--- back-end/WoopiAiHub.Application/Utils/AzureKeyVaultServices.cs
+++ back-end/WoopiAiHub.Application/Utils/AzureKeyVaultServices.cs
@@ -1,43 +1,61 @@
 ﻿using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using WoopiAiHub.Domain.Interfaces.Utils;
+using WoopiAiHub.Domain.Utils;
 
 namespace WoopiAiHub.Application.Utils
 {
     public class AzureKeyVaultServices : IKeyVaultServices
     {
         private readonly SecretClient _client;
+        private readonly KeyVaultSettings _settings;
 
-        public AzureKeyVaultServices(IConfiguration configuration)
+        public AzureKeyVaultServices(IOptions<KeyVaultSettings> options)
         {
-            var vaultUrl = configuration["Azure:VaultUrl"];
-            if (string.IsNullOrEmpty(vaultUrl))
+            _settings = options.Value;
+
+            if (string.IsNullOrEmpty(_settings.VaultUrl))
             {
-                throw new ArgumentNullException(nameof(vaultUrl));
+                throw new ArgumentNullException(nameof(_settings.VaultUrl));
             }
 
-            var tenantId = configuration["Azure:TenantId"];
-            var clientId = configuration["Azure:ClientId"];
-            var clientSecret = configuration["Azure:ClientSecret"];
-
-            if (string.IsNullOrWhiteSpace(clientSecret))
-                _client = new SecretClient(new Uri(vaultUrl), new DefaultAzureCredential()); // usa login local (CLI, VS, VS Code)
+            if (string.IsNullOrWhiteSpace(_settings.ClientSecret))
+                _client = new SecretClient(new Uri(_settings.VaultUrl), new DefaultAzureCredential()); // usa login local (CLI, VS, VS Code)
             else
-                _client = new SecretClient(new Uri(vaultUrl), new ClientSecretCredential(tenantId, clientId, clientSecret));
+                _client = new SecretClient(new Uri(_settings.VaultUrl), new ClientSecretCredential(_settings.TenantId, _settings.ClientId, _settings.ClientSecret));
         }
 
+
+        /// <summary>
+        /// Set a key in Azure vault
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public async Task SetSecretAsync(string key, string value)
         {
             await _client.SetSecretAsync(key, value);
         }
 
+        /// <summary>
+        /// Get a key in Azure vault
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public async Task<string?> GetSecretAsync(string key)
         {
             var secret = await _client.GetSecretAsync(key);
             return secret.Value.Value;
         }
 
+
+        /// <summary>
+        /// Delete a key in Azure vault 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public async Task DeleteSecretAsync(string key)
         {
             await _client.StartDeleteSecretAsync(key);
