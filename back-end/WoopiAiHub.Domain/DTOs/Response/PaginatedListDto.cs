d1c0f69f568cc4be00448a3f3509f493def8f888
ThiagoFragaK <thiagofraga@proton.me>
2025-10-24 18:58:44 -0300

    Add: pagination helper to be used globally

---
+++ back-end/WoopiAiHub.Domain/DTOs/Response/PaginatedListDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoopiAiHub.Domain.DTOs.Response
+{
+    public class PaginatedListDto<T>
+    {
+        public IEnumerable<T> Content { get; set; } = [];
+        public int CurrentPage { get; set; } = 1;
+        public int PageCount { get; set; } = 0;
+        public int RowCount { get; set; } = 0;
+        public int PageSize { get; set; } = 10;
+    }
+}
