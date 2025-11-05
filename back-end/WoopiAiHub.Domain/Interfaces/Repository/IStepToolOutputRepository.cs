4b38d16a759cc1b005217da91e4853789e7e610f
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2025-11-05 16:51:08 -0300

    Update IStepToolOutputRepository.cs

--- back-end/WoopiAiHub.Domain/Interfaces/Repository/IStepToolOutputRepository.cs
+++ back-end/WoopiAiHub.Domain/Interfaces/Repository/IStepToolOutputRepository.cs
@@ -7,5 +7,6 @@ namespace WoopiAiHub.Domain.Interfaces.Repository
         Task<bool> CreateAsync(StepToolOutput stepToolOutput);
         Task<List<StepToolOutput>> FindAllByStepToolListIdsAsync(IEnumerable<int> stepToolIds, int cardId);
         bool DeleteByIds(IEnumerable<int> ids);
+        Task<string> FindByStepToolId(int stepToolId, int cardId);
     }
 }
