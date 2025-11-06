8d01bde4f15e7005be245320c43641016c1684c3
ThiagoFragaK <thiagofraga@proton.me>
2025-11-06 19:11:15 -0300

    refactor: notification messages of removal in profile and teams

--- front-end/vueapp/src/components/management/teams/TeamsTable.vue
+++ front-end/vueapp/src/components/management/teams/TeamsTable.vue
@@ -149,24 +149,24 @@
                             this.$refs.DeleteDialog.close();
                             this.getTeams({ search: "", page: 1, type: null });
                             return this.$notify({
-                                title: 'team.title',
-                                message: 'team.deleteSuccess',
+                                title: 'management.teams.title',
+                                message: 'management.teams.deleteSuccess',
                                 variant: 'success',
                                 icon: 'CircleCheckBig',
                             });
                         }
                         else if (response.error.response.data.errorCode == 5) {
                              return this.$notify({
-                                title: 'team.title',
-                                message: 'team.deleteDocError',
+                                title: 'management.teams.title',
+                                message: 'management.teams.deleteDocError',
                                 variant: 'danger',
                                 icon: 'CircleX',
                             });
                         }
 
                         this.$notify({
-                            title: 'team.title',
-                            message: 'team.deleteError',
+                            title: 'management.teams.title',
+                            message: 'management.teams.deleteError',
                             variant: 'danger',
                             icon: 'CircleX',
                         });
