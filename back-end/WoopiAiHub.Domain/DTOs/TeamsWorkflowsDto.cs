dba5b732ce6548d9834174a78e02910e406dadbd
ThiagoFragaK <thiagofraga@proton.me>
2025-11-07 17:08:34 -0300

    refactor: using record in the dto

--- back-end/WoopiAiHub.Domain/DTOs/TeamsWorkflowsDto.cs
+++ back-end/WoopiAiHub.Domain/DTOs/TeamsWorkflowsDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace WoopiAiHub.Domain.DTOs
 {
-    public class TeamsWorkflowsDto
+    public record class TeamsWorkflowsDto
     {
         public int TeamId { get; set; }
         public List<int> Workflows { get; set; } = new List<int>();
