3041ca1f8e9a87593f6f9b3e389bf1e2f74fc1b2
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2025-11-10 17:26:08 -0300

    Update extracted-fields.vue

--- front-end/vueapp/src/components/pages/analyzer/extracted-fields.vue
+++ front-end/vueapp/src/components/pages/analyzer/extracted-fields.vue
@@ -19,28 +19,7 @@
                         {{ $t("labelEdited") }}
                     </span>
                 </div>
-                <div class="field-value-container" v-if=" field.outputType == 'N8N'">
-                    <input type="text"
-                           class="field-value"
-                           @input="(e) => handleFieldEdit(index, e.target.value)"
-                           :readonly="!isEditing[index]"
-                           v-model="fields[index].value" />
-                    <button v-if="!isEditing[index]"
-                            class="edit-button"
-                            @click="startEditing(index)"
-                            :title="$t('labelEdit')">
-                        <i class="fas fa-pen"></i>
-                    </button>
-                    <div v-else class="edit-actions">
-                        <button class="save-button" @click="saveEdit(index, field.outputId)" :title="$t('labelSave')">
-                            <i class="fas fa-check"></i>
-                        </button>
-                        <button class="cancel-button" @click="cancelEdit(index)" :title="$t('labelCancel')">
-                            <i class="fas fa-times"></i>
-                        </button>
-                    </div>
-                </div>
-                <div class="field-value-container" v-if=" field.outputType == 'N8N'">
+                <div class="field-value-container" v-if="field.outputType == 'N8N'">
                     <input type="text"
                            class="field-value"
                            @input="(e) => handleFieldEdit(index, e.target.value)"
