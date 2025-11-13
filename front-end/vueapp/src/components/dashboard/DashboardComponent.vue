d225b3d526391b13abb84d5d051df4b818e584c0
ThiagoFragaK <thiagofraga@proton.me>
2025-11-13 16:48:21 -0300

    Add: Dashboard component

---
+++ front-end/vueapp/src/components/dashboard/DashboardComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+        <div class="container-fluid scroll-area manage-user mx-2">
+            <h1>Dashboard</h1>
+            <TokensGraph />
+        </div>
+    </main>
+</template>
+
+<script>
+    import TokensGraph from '@/components/dashboard/graphs/TokensGraph.vue';
+    export default {
+        components: {
+            TokensGraph,
+        }
+    }
+</script>
