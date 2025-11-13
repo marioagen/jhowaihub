2dfead8b0884ea38226dfb44a166fdf898ee5269
ThiagoFragaK <thiagofraga@proton.me>
2025-11-13 16:47:02 -0300

    Add: Create bar graph global component

---
+++ front-end/vueapp/src/components/global/graphs/BarGraphComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <ApexChart
+            type="bar"
+            height="350"
+            :options="options"
+            :series="series"
+        />
+    </div>
+</template>
+
+<script>
+import ApexChart from 'vue3-apexcharts';
+export default {
+    name: 'BarGraphComponent',
+    components: { 
+        ApexChart
+    },
+    props: {
+        series: {
+            type: Object,
+            required: true,
+        },
+        options: {
+            type: Object,
+            required: true,
+        },
+    },
+}
+</script>
