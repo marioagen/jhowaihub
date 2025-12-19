716f5db615efdc9c0f77c9b95deeba52d219bc2b
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2025-12-19 11:52:07 -0300

    QA Notes

--- front-end/vueapp/src/pages/prompts/index.vue
+++ front-end/vueapp/src/pages/prompts/index.vue
@@ -33,62 +33,60 @@
     </main>
 </template>
 <script>
-    import PromptComponent from "@/components/prompts/PromptComponent.vue";
-    import PromptFilters from "@/components/prompts/PromptFilter"
-    export default {
-        name: "PromptPage",
-        emits: ['showAlertToast'],
-        data() {
-            return {
-                entitySearch: {},
-                resetInputSearch: false,
-                sidebarData: "Type",
-                queryPage: this.$route.query.page ? this.$route.query.page : 1,
-                searchInput: "",
-                searching: false,
-                dataPrompt: [],
-                loading: false,
-                pagination: { currentPage: 0, count: 0, totalPages: 0 },
-                modalAlertShow: false,
-                modalEntity: {},
-                isAscending: false,
-                dataModal: {},
-                colType: 2,
-                selectedOption: 9,
-                toastShow: false,
-                toastColor: "",
-                toastMessage: "",
-                listIds: [],
-                loadAllPrompts: true,
-            }
-        },
-        components: {
-            PromptComponent,
-            PromptFilters,
-        },
-        watch: {
-            filters: {
-                handler(newFilters) {
-                    this.$emit('filterData', newFilters);
-                },
-                deep: true
-            },
-            deep: true
-        },
-        methods: {
-            redirectToNewPrompt: function () {
-                this.$router.push({ name: "PromptNew" });
-            },
-            redirectToImport: function () {
-                this.$router.push({ name: "PromptImport" });
-            },
-            reloadData() {
-                this.$refs.PromptComponent.getList({ search: '', page: this.queryPage, type: null });
-            },
-            filterData(filters) {
-                this.$refs.PromptComponent.filters = filters;
-                this.reloadData();
+import PromptComponent from "@/components/prompts/PromptComponent.vue";
+import PromptFilters from "@/components/prompts/PromptFilter"
+export default {
+    name: "PromptPage",
+    emits: ['showAlertToast'],
+    data() {
+        return {
+            entitySearch: {},
+            resetInputSearch: false,
+            sidebarData: "Type",
+            queryPage: this.$route.query.page ? this.$route.query.page : 1,
+            searchInput: "",
+            searching: false,
+            dataPrompt: [],
+            loading: false,
+            pagination: { currentPage: 0, count: 0, totalPages: 0 },
+            modalAlertShow: false,
+            modalEntity: {},
+            isAscending: false,
+            dataModal: {},
+            colType: 2,
+            selectedOption: 9,
+            toastShow: false,
+            toastColor: "",
+            toastMessage: "",
+            listIds: [],
+            loadAllPrompts: true,
+        }
+    },
+    components: {
+        PromptComponent,
+        PromptFilters,
+    },
+    watch: {
+        filters: {
+            handler(newFilters) {
+                this.$emit('filterData', newFilters);
             },
         },
+    },
+    methods: {
+        redirectToNewPrompt: function () {
+            this.$router.push({ name: "PromptNew" });
+        },
+        redirectToImport: function () {
+            this.$router.push({ name: "PromptImport" });
+        },
+        reloadData() {
+            this.$refs.PromptComponent.getList({ search: '', page: this.queryPage, type: null });
+        },
+        filterData(filters) {
+            this.$refs.PromptComponent.filters = filters;
+            this.reloadData();
+        },
+    },
 }
 </script>
