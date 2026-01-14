3c469c2e879ae2f8fef84818f8aa7dae9a13a8fd
ThiagoFragaK <thiagofraga@proton.me>
2026-01-14 16:23:56 -0300

    Refactor date handling in UsageMonthServices and UsageUnitServices to utilize DateHelper for improved date parsing. Update repository and service interfaces to accept optional date filters for enhanced data retrieval.

--- back-end/WoopiAiHub.Repository/UsageUnitRepository.cs
+++ back-end/WoopiAiHub.Repository/UsageUnitRepository.cs
@@ -1,10 +1,12 @@
 using Google.Api;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Dynamic.Core;
+using WoopiAiHub.Domain.DTOs.Request;
 using WoopiAiHub.Domain.DTOs.Response;
 using WoopiAiHub.Domain.DTOs.Response.Automation;
 using WoopiAiHub.Domain.Interfaces.Repository;
 using WoopiAiHub.Domain.Models;
+using WoopiAiHub.Domain.Utils;
 using WoopiAiHub.Repository.Context;
 
 namespace WoopiAiHub.Repository
@@ -21,22 +23,39 @@ namespace WoopiAiHub.Repository
         /// <summary>
         /// Find all usage units
         /// </summary>
+        /// <param name="dateFilterDto"></param>
         /// <returns></returns>
-        public async Task<IEnumerable<UsageUnitDto>> FindAllAsync()
+        public async Task<IEnumerable<UsageUnitDto>> FindAllAsync(DateFilterDto? dateFilterDto = null)
         {
-            return await _context.UsageUnits
-                                 .AsNoTracking()
-                                 .Select(uu => new UsageUnitDto
-                                    {
-                                        Id = uu.Id,
-                                        Name = uu.Name,
-                                        UsageTypeId = uu.UsageTypeId,
-                                        UsageTypeName = uu.UsageType!.Name,
-                                        ModelEmbeddingId = uu.ModelEmbeddingId,
-                                        ModelEmbeddingName = uu.ModelEmbedding!.Name,
-                                        Value = uu.Value
-                                 })
-                                 .ToListAsync();
+            var query = _context.UsageUnits.AsNoTracking();
+
+            if (dateFilterDto != null)
+            {
+                var startDate = DateHelper.ParseDate(dateFilterDto.Start);
+                if (startDate.HasValue)
+                {
+                    query = query.Where(x => x.Created.Date >= startDate.Value.Date);
+                }
+
+                var endDate = DateHelper.ParseDate(dateFilterDto.End);
+                if (endDate.HasValue)
+                {
+                    query = query.Where(x => x.Created.Date <= endDate.Value.Date);
+                }
+            }
+
+            return await query
+                .Select(uu => new UsageUnitDto
+                {
+                    Id = uu.Id,
+                    Name = uu.Name,
+                    UsageTypeId = uu.UsageTypeId,
+                    UsageTypeName = uu.UsageType!.Name,
+                    ModelEmbeddingId = uu.ModelEmbeddingId,
+                    ModelEmbeddingName = uu.ModelEmbedding!.Name,
+                    Value = uu.Value
+                })
+                .ToListAsync();
         }
     }
 }
