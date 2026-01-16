a03af6a68b79d7408cfb870886404f50d1868387
brendaLessa <blalmeida@latam.stefanini.com>
2026-01-16 16:48:11 -0300

    first-commit

--- front-end/vueapp/src/locales/translations/pt.js
+++ front-end/vueapp/src/locales/translations/pt.js
@@ -113,7 +113,7 @@
                 "Este documento está associado a múltiplas esteiras de processamento.<br>Escolha qual deseja visualizar:",
             searchPlaceholder: "Buscar esteira de processamento...",
             clickToView: "Clique para visualizar",
-            nothingFound: "Nenhuma esteira de processamento encontrada.",
+            nothingFound: "Nenhuma esteira de processamento encontrada. Caso não há nenhuma esteira vinculada, considere reenviar o arquivo vinculando uma esteira de processamento",
             cancel: "Cancelar",
             errorToGetWorkflows: "Erro ao buscar esteiras de processamento.",
             errorUnexpected: "Erro inesperado.",
