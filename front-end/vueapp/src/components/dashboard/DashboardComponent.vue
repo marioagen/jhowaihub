e9cae952c31683a982a02d91084ffaffc643e42d
ThiagoFragaK <thiagofraga@proton.me>
2026-01-21 14:52:14 -0300

    Remove console log from filterData method in DashboardComponent.vue to clean up code and improve performance.

--- front-end/vueapp/src/components/dashboard/DashboardComponent.vue
+++ front-end/vueapp/src/components/dashboard/DashboardComponent.vue
@@ -174,7 +174,6 @@
             filterData(filters) {
                 this.filters = filters;
                 this.totalWTC = 0;
-                console.log("Filter Data", this.filters);
                 this.$refs.TokensGraph.updateGraph(
                     this.filters.start,
                     this.filters.end
