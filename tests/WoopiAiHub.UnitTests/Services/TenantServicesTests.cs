934c8d7fbbef7898560fef524537550a3271403d
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2026-01-23 09:41:06 -0300

    Update TenantServicesTests.cs

--- tests/WoopiAiHub.UnitTests/Services/TenantServicesTests.cs
+++ tests/WoopiAiHub.UnitTests/Services/TenantServicesTests.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Moq.AutoMock;
+using System.Data.Common;
 using WoopiAiHub.Application.Services;
 using WoopiAiHub.Domain.DTOs;
+using WoopiAiHub.Domain.DTOs.Messaging;
 using WoopiAiHub.Domain.Interfaces.Refit;
+using WoopiAiHub.Domain.Interfaces.Repository.Cache;
+using WoopiAiHub.Domain.Interfaces.Services;
 using WoopiAiHub.Domain.Interfaces.Utils;
+using WoopiAiHub.Repository.Context;
 using WoopiAiHub.UnitTests.Fixture;
 using Xunit;
 
@@ -37,6 +44,8 @@ namespace WoopiAiHub.UnitTests.Services
             _mocker.Use(httpContext.Object);
 
             var httpContextAccessor = new Mock<IHttpContextAccessor>();
+            httpContextAccessor.SetupAllProperties();
+            httpContextAccessor.Object.HttpContext = new DefaultHttpContext();
             httpContextAccessor.Setup(x => x.HttpContext).Returns(httpContext.Object);
             _mocker.Use(httpContextAccessor.Object);
 
@@ -51,6 +60,44 @@ namespace WoopiAiHub.UnitTests.Services
             apiDependenciesMock.SetupGet(x => x.MarketPlaceApi).Returns(_mocker.GetMock<IMarketPlaceApi>().Object);
             apiDependenciesMock.SetupGet(x => x.KeyGeneratorApi).Returns(_mocker.GetMock<IKeyGeneratorApi>().Object);
             _mocker.Use(apiDependenciesMock.Object);
+
+            var _serviceProviderMock = new Mock<IServiceProvider>();
+            var _userServiceMock = new Mock<IUserServices>();
+            var _dbConnectionMock = new Mock<DbConnection>();
+
+            // 1. Configurar o DbContext Real com Conexão Mockada
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlServer(_dbConnectionMock.Object)
+                .Options;
+
+            // Se o seu ApplicationDbContext tiver um construtor que aceita Options
+            var _dbContext = new ApplicationDbContext(options);
+
+            // 2. Configurar a Hierarquia de Escopo do ServiceProvider
+            var scopeMock = new Mock<IServiceScope>();
+            var scopeServiceProviderMock = new Mock<IServiceProvider>();
+
+            // O provedor do escopo deve retornar o DbContext e o UserService
+            scopeServiceProviderMock
+                .Setup(x => x.GetService(typeof(ApplicationDbContext)))
+                .Returns(_dbContext);
+
+            scopeServiceProviderMock
+                .Setup(x => x.GetService(typeof(IUserServices)))
+                .Returns(_userServiceMock.Object);
+
+            scopeMock.Setup(x => x.ServiceProvider).Returns(scopeServiceProviderMock.Object);
+
+            var scopeFactoryMock = new Mock<IServiceScopeFactory>();
+            scopeFactoryMock
+                .Setup(x => x.CreateScope())
+                .Returns(scopeMock.Object);
+
+            _serviceProviderMock
+                .Setup(x => x.GetService(typeof(IServiceScopeFactory)))
+                .Returns(scopeFactoryMock.Object);
+
+            _mocker.Use(_serviceProviderMock.Object);
 
             _tenantServices = _mocker.CreateInstance<TenantServices>();
         }
@@ -105,6 +152,41 @@ namespace WoopiAiHub.UnitTests.Services
             // Act & Assert
             var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _tenantServices.InitializeTenant(tenant));
             Assert.Equal("KeyAccess is not configured in the application settings.", exception.Message);
+        }
+
+        [Fact(DisplayName = "ProcessSubscription should create database and notify marketplace when subscription is valid")]
+        [Trait("ProcessSubscription", "Success")]
+        public void ProcessSubscription_ShouldCreateTenantSuccessfully()
+        {
+            // Arrange
+            var tenantSubscriptionDto = new TenantSubscriptionDto
+            {
+                MarketplaceId = Guid.NewGuid(),
+                Name = "tenant-name",
+                Email = "owner@example.com",
+                IsActive = true,
+                PlanName = "basic",
+                DataBaseName = "tenant_db",
+                DateStart = DateTime.UtcNow
+            };
+
+            var tenantRepositoryMock = _mocker.GetMock<ITenantRepository>();
+            tenantRepositoryMock
+                .Setup(r => r.CreateDatabase())
+                .Returns(true);
+
+
+            var marketPlaceApiMock = _mocker.GetMock<IMarketPlaceApi>();
+            marketPlaceApiMock
+                .Setup(m => m.SendDatabaseCreatedNotification(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(true);
+
+            // Act
+            _tenantServices.ProcessSubscription(tenantSubscriptionDto);
+
+            // Assert
+            tenantRepositoryMock.Verify(r => r.CreateDatabase(), Times.Once);
+            marketPlaceApiMock.Verify(m => m.SendDatabaseCreatedNotification(It.IsAny<string>(), tenantSubscriptionDto.Name), Times.Once);
         }
 
     }
