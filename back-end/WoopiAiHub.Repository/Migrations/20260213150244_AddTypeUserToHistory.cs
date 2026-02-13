7c0d6aebb9c1fc52bbe5a4388991b476af894eaf
ThiagoFragaK <thiagofraga@proton.me>
2026-02-13 16:24:09 -0300

    Add migration to include Type and UserId in DocumentHistories: Introduce new columns and foreign key relationship in the DocumentHistories table to enhance tracking of user actions.

--- back-end/WoopiAiHub.Repository/Migrations/20260213150244_AddTypeUserToHistory.cs
+++ back-end/WoopiAiHub.Repository/Migrations/20260213150244_AddTypeUserToHistory.cs
@@ -1,12 +1,11 @@
-using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace WoopiAiHub.Repository.Migrations
 {
     /// <inheritdoc />
-    public partial class AddTypeAndUserIdToDocumentHistory : Migration
+    public partial class AddTypeUserToHistory : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
