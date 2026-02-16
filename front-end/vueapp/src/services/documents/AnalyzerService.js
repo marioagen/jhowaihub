cfbc62bab296ea91f367dc3bc552361ee0e40812
ThiagoFragaK <thiagofraga@proton.me>
2026-02-16 16:14:04 -0300

    Add AnalyzerService for document questionnaire handling and extend QuizzesService with getQuizzes method

---
+++ front-end/vueapp/src/services/documents/AnalyzerService.js
@@ -0,0 +1,28 @@
+import api from "@/services/api";
+
+export default {
+    sendQuizz(params) {
+        return api
+            .post("/Document/inputQuestionnaire", params)
+            .then(() => {
+                return true;
+            })
+            .catch((error) => {
+                return {
+                    error: error,
+                };
+            });
+    },
+    submitInput(params) {
+        return api
+            .post("/Document/input/", params)
+            .then((response) => {
+                return response;
+            })
+            .catch((error) => {
+                return {
+                    error: error,
+                };
+            });
+    },
+};
--- front-end/vueapp/src/services/quizzes/QuizzesService.js
+++ front-end/vueapp/src/services/quizzes/QuizzesService.js
@@ -2,7 +2,8 @@ import api from "@/services/api";
 
 export default {
     getQuizzes(params) {
-        return api.get("/Questionnaire/Paged", { params: params })
+        return api
+            .get("/Questionnaire/Paged", { params: params })
             .then(({ data }) => {
                 return {
                     content: data.content,
@@ -11,28 +12,42 @@ export default {
                         totalPages: data.pageCount,
                         rowCount: data.rowCount,
                         totalItems: data.rowCount,
-                    }
-                }
+                    },
+                };
             })
             .catch((e) => {
                 return {
                     error: e,
-                }
+                };
             });
     },
-    getQuizzById(id) {
-        return api.get(`/Questionnaire/${id}`)
+    getQuizzes() {
+        return api
+            .get("/Questionnaire/FindAll")
             .then(({ data }) => {
                 return data;
             })
             .catch((e) => {
                 return {
                     error: e,
-                }
+                };
+            });
+    },
+    getQuizzById(id) {
+        return api
+            .get(`/Questionnaire/${id}`)
+            .then(({ data }) => {
+                return data;
+            })
+            .catch((e) => {
+                return {
+                    error: e,
+                };
             });
     },
     createQuizz(params) {
-        return api.post("/Questionnaire", params)
+        return api
+            .post("/Questionnaire", params)
             .then(() => {
                 return true;
             })
@@ -44,7 +59,8 @@ export default {
             });
     },
     editQuizz(params) {
-        return api.put("/Questionnaire", params)
+        return api
+            .put("/Questionnaire", params)
             .then(() => {
                 return true;
             })
@@ -56,14 +72,15 @@ export default {
             });
     },
     deleteQuizzById(ids) {
-        return api.delete("/Questionnaire/DeleteByIds", { data: ids })
+        return api
+            .delete("/Questionnaire/DeleteByIds", { data: ids })
             .then(() => {
                 return true;
             })
             .catch(function (e) {
                 return {
                     error: e,
-                }
+                };
             });
-    }
-}
+    },
+};
