97447810172c0ad1f443636aac69e53bf1632498
ThiagoFragaK <thiagofraga@proton.me>
2026-02-25 17:05:12 -0300

    Fix: UsageArchiveService Test

--- back-end/WoopiAiHub.Application/Services/UsageArchiveService.cs
+++ back-end/WoopiAiHub.Application/Services/UsageArchiveService.cs
@@ -167,6 +167,12 @@ namespace WoopiAiHub.Application.Services
             }
 
             var usageByTenant = await usageMonthRepository.FindTotalUsageAsync(lastPeriod.PeriodStart, lastPeriod.PeriodEnd);
+            if (usageByTenant == 0)
+            {
+                _logger.LogDebug("No usage found for tenant {TenantName} in period {PeriodStart}-{PeriodEnd}, skipping consumption report", tenant.Name, lastPeriod.PeriodStart, lastPeriod.PeriodEnd);
+                return;
+            }
+
             var tenantConsumption = new TenantConsumptionDto
             {
                 Tenant = tenant.Name,
