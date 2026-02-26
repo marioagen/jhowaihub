ffd33ad165b6d98c9ae3db014bd1624dbdfc141a
ThiagoFragaK <thiagofraga@proton.me>
2026-02-25 21:17:51 -0300

    Add: AuditCard ActionType Enum

---
+++ back-end/WoopiAiHub.Domain/Enum/Audit/AuditCardActionType.cs
@@ -0,0 +1,15 @@
+namespace WoopiAiHub.Domain.Enum.Audit
+{
+    public enum AuditCardActionType
+    {
+        Upload = 0,
+        Assign = 1,
+        Unassign = 2,
+        Advancement = 3,
+        EditAnswer = 4,
+        AnalyzeApproval = 5,
+        AnalyzeRejection = 6,
+        Finalize = 7,
+        Removed = 8
+    }
+}
