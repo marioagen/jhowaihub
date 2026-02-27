3a7911aae6aef5dd57034c8100ad08cc924f929c
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2026-02-27 17:19:16 -0300

    Update WorkflowController.cs

--- back-end/WoopiAiHub.Api/Controllers/Internal/WorkflowController.cs
+++ back-end/WoopiAiHub.Api/Controllers/Internal/WorkflowController.cs
@@ -13,6 +13,10 @@ namespace WoopiAiHub.Api.Controllers.Internal
     {
         private readonly IWorkflowServices _workflowServices = workflowServices;
 
+        /// <summary>
+        /// Returns all workflows in a basic format, which includes only the essential information needed for internal use. This endpoint is intended for internal use only and should not be exposed to external clients.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [SwaggerOperation("Endpoint that receives the request to return all workflow in basic format")]
         [ProducesResponseType(typeof(ICollection<WorkflowInternalDto>), StatusCodes.Status200OK)]
