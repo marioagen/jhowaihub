1ebf3743b5ffd17598ac24f1c7ea90001daac7ff
brendaLessa <blalmeida@latam.stefanini.com>
2026-03-20 17:29:26 -0300

    first-commit

--- front-end/vueapp/src/components/documentsHub/workflows/WorkflowsKanbanComponent.vue
+++ front-end/vueapp/src/components/documentsHub/workflows/WorkflowsKanbanComponent.vue
@@ -33,7 +33,7 @@
                                     <LucideIcon
                                         icon="ChevronDown"
                                         :size="20"
-                                        class="ms-2"
+                                        class="ms-2 text-muted"
                                     />
                                 </button>
                                 <ul class="dropdown-menu p-2 workflow-list">
