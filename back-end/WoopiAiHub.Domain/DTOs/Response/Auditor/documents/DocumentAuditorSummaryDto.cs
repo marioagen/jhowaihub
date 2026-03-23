f9158c7647bce7fa5a34f6f7af9515b0719e2316
Thiago Fraga <tcfraga@stefanini.com>
2026-03-23 11:05:33 -0300

    Remove obsolete summary comment from DocumentAuditorSummaryRowDto.cs to improve code clarity.

--- back-end/WoopiAiHub.Domain/DTOs/Response/Auditor/documents/DocumentAuditorSummaryDto.cs
+++ back-end/WoopiAiHub.Domain/DTOs/Response/Auditor/documents/DocumentAuditorSummaryDto.cs
@@ -7,8 +7,6 @@ namespace WoopiAiHub.Domain.DTOs.Response.Auditor.Documents
         public IReadOnlyList<DocumentAuditorWorkflowsDto> Workflows { get; init; } = [];
         public int ActionsCount { get; init; }
         public bool IsFinalized { get; init; }
-
-        /// <summary>True when the document is soft-deleted; UI should show as removed (e.g. red badge).</summary>
         public bool IsRemoved { get; init; }
     }
 }
