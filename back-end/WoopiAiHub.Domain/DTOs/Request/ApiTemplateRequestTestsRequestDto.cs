3b20c8863b58e5882de45151dcfa2e26f126ca32
ThiagoFragaK <thiagofraga@proton.me>
2026-03-30 15:34:35 -0300

    Refactor: Remove blank spots

--- back-end/WoopiAiHub.Domain/DTOs/Request/ApiTemplateRequestTestsRequestDto.cs
+++ back-end/WoopiAiHub.Domain/DTOs/Request/ApiTemplateRequestTestsRequestDto.cs
@@ -3,11 +3,8 @@ namespace WoopiAiHub.Domain.DTOs.Request
     public class ApiTemplateRequestTestsRequestDto
     {
         public int? TemplateId { get; set; }
-
         public ApiTemplateCreateDto? Draft { get; set; }
-
         public Dictionary<string, string>? Variables { get; set; }
-
         public string? TemplateName { get; set; }
         public string? Tenant { get; set; }
         public string? Email { get; set; }
