39ba21d0fa3d77e7708862bb63ed85571d070f50
ThiagoFragaK <thiagofraga@proton.me>
2026-04-09 19:22:14 -0300

    Refactor: changed collapse to ajust for the accordion component

--- front-end/vueapp/src/components/global/CollapseComponent.vue
+++ front-end/vueapp/src/components/global/CollapseComponent.vue
@@ -1,44 +1,63 @@
 <template>
-    <div 
-        class="collapse" 
-        :id="collapseId" 
+    <div
+        :class="collapseRootClass"
+        :id="collapseId"
         ref="collapseEl"
+        :data-bs-parent="parentSelector || undefined"
+        :aria-labelledby="labelledBy || undefined"
     >
-        <div class="card card-body border-light bg-transparent rounded-3">
+        <div :class="innerWrapperClass">
             <slot />
         </div>
     </div>
 </template>
-
 <script>
     export default {
         name: "CollapseComponent",
         props: {
-            collapseId: { 
-                type: String, 
-                required: true 
-            }
+            collapseId: {
+                type: String,
+                required: true,
+            },
+            parentSelector: {
+                type: String,
+                default: null,
+            },
+            collapseRootClass: {
+                type: String,
+                default: "collapse",
+            },
+            innerWrapperClass: {
+                type: String,
+                default: "card card-body border-light bg-transparent rounded-3",
+            },
+            labelledBy: {
+                type: String,
+                default: null,
+            },
         },
         mounted() {
-            this.collapseInstance = new window.bootstrap.Collapse(this.$refs.collapseEl, {
-                toggle: false
-            })
+            const options = { toggle: false };
+            if (this.parentSelector) {
+                options.parent = this.parentSelector;
+            }
+            this.collapseInstance = new window.bootstrap.Collapse(this.$refs.collapseEl, options);
         },
         beforeUnmount() {
             if (this.collapseInstance) {
-                this.collapseInstance.dispose()
+                this.collapseInstance.dispose();
             }
         },
         methods: {
             open() {
-                this.collapseInstance.show()
+                this.collapseInstance.show();
             },
             close() {
-                this.collapseInstance.hide()
+                this.collapseInstance.hide();
             },
             toggle() {
-                this.collapseInstance.toggle()
-            }
-        }
-    }
+                this.collapseInstance.toggle();
+            },
+        },
+    };
 </script>
