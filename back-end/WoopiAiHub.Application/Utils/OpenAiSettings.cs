14671ee48944e26bebebd47db720d22cfcb3ae0f
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2026-05-07 08:14:22 -0300

    OpenAi MaxToolCalls

--- back-end/WoopiAiHub.Application/Utils/OpenAiSettings.cs
+++ back-end/WoopiAiHub.Application/Utils/OpenAiSettings.cs
@@ -3,6 +3,7 @@ namespace WoopiAiHub.Application.Utils
 {
     public class OpenAiSettings
     {
+        public int MaxToolCalls { get; set; }
         public double Temperature { get; set; }
         public string Model { get; set; } = string.Empty;
         public string ApiVersion { get; set; } = string.Empty;
