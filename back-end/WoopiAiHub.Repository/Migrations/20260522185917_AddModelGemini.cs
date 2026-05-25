c6438b0b2683ddc1ff9a82225ee2aa93ec6521c3
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2026-05-25 13:53:54 -0300

    Update 20260522185917_AddModelGemini.cs

--- back-end/WoopiAiHub.Repository/Migrations/20260522185917_AddModelGemini.cs
+++ back-end/WoopiAiHub.Repository/Migrations/20260522185917_AddModelGemini.cs
@@ -11,24 +11,24 @@ namespace WoopiAiHub.Repository.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-               IF NOT EXISTS (SELECT 1 FROM ModelEmbeddings WHERE [Name] = 'gemini-2.5-pro')
+               IF NOT EXISTS (SELECT 1 FROM ModelEmbeddings WHERE [Name] = 'gemini-flash-latest')
                BEGIN
                    INSERT INTO ModelEmbeddings (Name, Created)
-                   VALUES ('gemini-2.5-pro', GETDATE());
+                   VALUES ('gemini-flash-latest', GETDATE());
                END
            ");
 
             migrationBuilder.Sql(@"
-               DECLARE @EmbeddingIdDeepSeek INT;
+               DECLARE @EmbeddingIdGeminiFlashLatest INT;
                DECLARE @UsageTypeIdToken INT;
 
-               SELECT @EmbeddingIdDeepSeek = Id FROM ModelEmbeddings WHERE [Name] = 'gemini-2.5-pro';
+               SELECT @EmbeddingIdGeminiFlashLatest = Id FROM ModelEmbeddings WHERE [Name] = 'gemini-flash-latest';
                SELECT @UsageTypeIdToken = Id FROM UsageTypes WHERE [Name] = 'Token';
 
-               IF NOT EXISTS (SELECT 1 FROM UsageUnits WHERE [UsageTypeId] = @UsageTypeIdToken AND [ModelEmbeddingId] = @EmbeddingIdDeepSeek)
+               IF NOT EXISTS (SELECT 1 FROM UsageUnits WHERE [UsageTypeId] = @UsageTypeIdToken AND [ModelEmbeddingId] = @EmbeddingIdGeminiFlashLatest)
                BEGIN
                    INSERT INTO UsageUnits (Name, UsageTypeId, ModelEmbeddingId, Value, Created)
-                   VALUES ('Unit', @UsageTypeIdToken, @EmbeddingIdDeepSeek, 0.000000790,  GETDATE());
+                   VALUES ('Unit', @UsageTypeIdToken, @EmbeddingIdGeminiFlashLatest, 0.000000790,  GETDATE());
                END
            ");
         }
