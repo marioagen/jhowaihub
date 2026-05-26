c907d921c882bebab1af548b9df19759e5b9cf1d
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2026-05-26 14:59:05 -0300

    Disable permissions

---
+++ back-end/WoopiAiHub.Repository/Migrations/20260526163640_RemoveUnusePermissions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WoopiAiHub.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUnusePermissions : Migration
+    {
+        private const string ObsoletePermissionDescriptions = @"
+                    'permissions.descriptions.questions',
+                    'permissions.descriptions.types',
+                    'permissions.descriptions.quizzes',
+                    'permissions.descriptions.prompts'
+            ";
+
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+                UPDATE Permissions
+                SET [Active] = 0
+                WHERE [Description] IN ({ObsoletePermissionDescriptions});
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+                UPDATE Permissions
+                SET [Active] = 1
+                WHERE [Description] IN ({ObsoletePermissionDescriptions});
+            ");
+        }
+    }
+}
