8d8aa18107ceb0fde9fb1f40074a1d7efd85e888
CESAR JOAO ANDREAZZA <cjandreazza@stefanini.com>
2026-05-28 16:45:22 -0300

    Total permission

--- back-end/WoopiAiHub.Repository/PermissionRepository.cs
+++ back-end/WoopiAiHub.Repository/PermissionRepository.cs
@@ -7,7 +7,7 @@ namespace WoopiAiHub.Repository
 {
     public class PermissionRepository : IPermissionRepository
     {
-        private const string WorkflowStepGroup = "permissions.groups.workflowStep";
+        private const string WorkflowStepGroup = "Workflow-Step";
         private readonly Context.ApplicationDbContext _context;
         public PermissionRepository(Context.ApplicationDbContext context)
         {
